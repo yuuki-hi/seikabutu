37cc3d1cabaa87f92c7d227648d0f767b740665b
yuuki-hi <bino723ydk@icloud.com>
2022-07-30 11:01:44 +0900

    ブログ投稿のバリデーション機能を追加

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,27 +1,24 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Blog</title>
-
-        <!-- Fonts -->
-        <!--<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">-->
-
     </head>
     <body>
-        <form action="/posts" method="post">
+        <h1>Blog Name</h1>
+        <form action="/posts" method="POST">
             @csrf
-            <div>
+            <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]">
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
-            <div>
+            <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]"></textarea>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old('post.body') }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
-            <div><input type="submit" value="保存する"></div>
+            <input type="submit" value="保存"/>
         </form>
         <div class="back">[<a href="/">back</a>]</div>
     </body>
