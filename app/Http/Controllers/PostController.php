4cd765dd89efbdeeb19b6487f29a1de116d3bede
yuuki-hi <bino723ydk@icloud.com>
2022-08-06 11:33:02 +0900

    削除機能実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,4 +36,9 @@ class PostController extends Controller
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
+    public function delete(Post $post)    {
+        $post->delete();
+        return redirect('/');
+    }
 }
